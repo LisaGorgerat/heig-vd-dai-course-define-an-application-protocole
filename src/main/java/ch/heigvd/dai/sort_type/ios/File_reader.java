f4d5ff15d318b5da543d1722713cde82d87e4cf2
AlexB-HEIG <alex.berberat@heig-vd.ch>
2024-10-19 14:33:35 +0200

    Reader Writer and Apparition done

--- src/main/java/ch/heigvd/dai/sort_type/ios/File_reader.java
+++ src/main/java/ch/heigvd/dai/sort_type/ios/File_reader.java
@@ -1,4 +1,45 @@
-package ch.heigvd.dai.ios;
+package ch.heigvd.dai.sort_type.ios;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class File_reader {
+    public static Map<String, Integer> words_extractor(String filename) {
+
+        //Map<String, Integer> words_list = new HashMap<>();
+        Map<String, Integer> words_list = new LinkedHashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename, StandardCharsets.UTF_8))) {
+
+            while ((br.ready())) {
+                String line = br.readLine();
+
+                line = line.replaceAll("\\p{Punct}|\\d", " ");
+                line = line.replaceAll("–|’|«|»", " "); // Needed because of non-standard char
+
+                line = line.trim().replaceAll(" +", " ");
+
+                if (line.isBlank()) {
+                    continue;
+                }
+
+                line = line.toLowerCase();
+
+                for (String word : line.split(" ")) {
+                    if (words_list.containsKey(word)) {
+                        words_list.put(word, words_list.get(word) + 1);
+                    } else {
+                        words_list.put(word, 1);
+                    }
+                }
+            }
+            
+        } catch (java.io.IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+
+        return words_list;
+    }
 }
