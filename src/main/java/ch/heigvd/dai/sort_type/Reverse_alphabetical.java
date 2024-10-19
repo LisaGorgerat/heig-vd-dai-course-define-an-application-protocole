8e1ef1c0a51d3f5fa66b715503517a09431da639
AlexB-HEIG <alex.berberat@heig-vd.ch>
2024-10-19 16:34:44 +0200

    First try for Reverse alpha sort

--- src/main/java/ch/heigvd/dai/sort_type/Reverse_alphabetical.java
+++ src/main/java/ch/heigvd/dai/sort_type/Reverse_alphabetical.java
@@ -1,11 +1,24 @@
 package ch.heigvd.dai.sort_type;
 
 import ch.heigvd.dai.sort_type.Sortable;
+import ch.heigvd.dai.sort_type.ios.*;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Reverse_alphabetical implements Sortable {
     @Override
-    public void sort(String filename){
+    public void sort(String filename) {
 
+
+
+        Map<String, Integer> sorted_words = new TreeMap<>(Comparator.reverseOrder());
+        sorted_words.putAll(File_reader.words_extractor(filename));
+
+
+        File_writer.output_generator(filename, "Reverse Alphabetical", sorted_words);
 
     }
 }
