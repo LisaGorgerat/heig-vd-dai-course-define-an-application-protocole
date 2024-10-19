812c611198f4181ffd8b16f9c649e5e04ce1b0ad
AlexB-HEIG <alex.berberat@heig-vd.ch>
2024-10-19 18:08:39 +0200

    all sort algo finished

--- src/main/java/ch/heigvd/dai/sort_type/Reverse_alphabetical.java
+++ src/main/java/ch/heigvd/dai/sort_type/Reverse_alphabetical.java
@@ -3,7 +3,6 @@ package ch.heigvd.dai.sort_type;
 import ch.heigvd.dai.sort_type.Sortable;
 import ch.heigvd.dai.sort_type.ios.*;
 
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
@@ -12,13 +11,9 @@ public class Reverse_alphabetical implements Sortable {
     @Override
     public void sort(String filename) {
 
-
-
         Map<String, Integer> sorted_words = new TreeMap<>(Comparator.reverseOrder());
         sorted_words.putAll(File_reader.words_extractor(filename));
 
-
         File_writer.output_generator(filename, "Reverse Alphabetical", sorted_words);
-
     }
 }
--- src/main/java/ch/heigvd/dai/sort_type/Usage.java
+++ src/main/java/ch/heigvd/dai/sort_type/Usage.java
@@ -1,11 +1,29 @@
 package ch.heigvd.dai.sort_type;
 
 import ch.heigvd.dai.sort_type.Sortable;
+import ch.heigvd.dai.sort_type.ios.*;
+
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Usage implements Sortable {
     @Override
-    public void sort(String filename){
+    public void sort(String filename) {
+
+        Map<String, Integer> words_list = File_reader.words_extractor(filename);
+
+        // Sort the map by value - source: https://howtodoinjava.com/java/sort/java-sort-map-by-values/
+        Map<String, Integer> sorted_words = words_list.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
 
 
+        File_writer.output_generator(filename, "Usage", sorted_words);
     }
 }
